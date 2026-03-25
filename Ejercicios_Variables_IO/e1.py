ed8268f899ff707533382dd54a69697b7cddb390
neuraljules <neuraljules@gmail.com>
2026-03-25 15:10:50 -0600

    Commit inicial: e1

---
+++ Ejercicios_Variables_IO/e1.py
@@ -0,0 +1,16 @@
+# Programa python donde tienes que imprimir tu informacion personal.
+
+nombre = "Julio"
+apellido = "García"
+edad = 33
+codigo_postal = "45700"
+ciudad = "Zapopan"
+estado = "Jalisco"
+
+print("Nombre: ", nombre)
+print("Apellido:", apellido)
+print("Edad: ", edad)
+print("Codigo postal: ", codigo_postal)
+print("Ciudad: ", ciudad)
+print("Estado: ", estado)
+
