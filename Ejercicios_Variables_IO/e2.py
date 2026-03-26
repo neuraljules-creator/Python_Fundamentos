958c7e019f48573f93ecedc71920c85d1d51b89a
neuraljules <neuraljules@gmail.com>
2026-03-26 13:11:22 -0600

    solucion de e2.py

---
+++ Ejercicios_Variables_IO/e2.py
@@ -0,0 +1,8 @@
+# una empres determina que la ganancia anual es el 23% de las ventas anuales
+# escribe un programa que pide al usuario el monto de las ventas totales y calcula la ganancia anual
+
+venta_anual = float(input("Ingrese el monto de las ventas anuales: "))
+
+ganancia = venta_anual * 0.23
+
+print("La ganancia total o profit es: ", ganancia)
